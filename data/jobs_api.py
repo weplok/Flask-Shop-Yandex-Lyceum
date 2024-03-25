6328481d1ae836631e91829b05a1152f53e05fd2
MaximShushas <jMax.Nemo@yandex.ru>
2024-03-25 13:21:43 +0300

    Update Rest-api Add GET one-job

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -9,26 +9,41 @@ blueprint = flask.Blueprint(
     template_folder='templates'
 )
 
+job_params = (
+    'id',
+    'team_leader',
+    'user.name',
+    'user.surname',
+    'job',
+    'work_size',
+    'collaborators',
+    'start_date',
+    'end_date',
+    'is_finished',
+)
+
 
 @blueprint.route('/api/jobs')
-def get_news():
+def get_jobs():
+    global job_params
     db_sess = db_session.create_session()
-    news = db_sess.query(Jobs).all()
+    jobs = db_sess.query(Jobs).all()
     return flask.jsonify(
         {
-            'news':
-                [item.to_dict(only=(
-                    'id',
-                    'team_leader',
-                    'user.name',
-                    'user.surname',
-                    'job',
-                    'work_size',
-                    'collaborators',
-                    'start_date',
-                    'end_date',
-                    'is_finished',
-                ))
-                    for item in news]
+            'jobs':
+                [item.to_dict(only=job_params) for item in jobs]
+        }
+    )
+
+
+@blueprint.route('/api/jobs/<int:job_id>', methods=['GET'])
+def get_one_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(job_id)
+    if not job:
+        return flask.make_response(flask.jsonify({'error': 'Not found'}), 404)
+    return flask.jsonify(
+        {
+            'job': job.to_dict(only=job_params)
         }
     )
